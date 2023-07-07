d247d88d90fe949b75e557562fca0af088b021ab
santiarroyave <santi98cv@hotmail.com>
2023-07-07 15:23:49 +0200

    Nuevo proyecto

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'sao-fe-gc-ejercicios-T14-Angular-07-2023';
+}
